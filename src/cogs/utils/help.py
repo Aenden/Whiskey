e1a814c5c78535df84170db7a9dd863f1fee6be0
Aenden <98803639+Aenden@users.noreply.github.com>
2022-02-01 13:45:23 +0530

    Create help.py

---
+++ src/cogs/utils/help.py
@@ -0,0 +1,35 @@
+from difflib import get_close_matches
+from discord.ext import commands
+import discord
+
+class HelpCommand(commands.HelpCommand):
+    def __init__(self):
+        super().__init__(
+            command_attrs={
+                "help": "Shows help about the bot, a command, or a category",
+            },
+            verify_checks=False,
+        )
+
+    async def send_bot_help(self, mapping):
+      ctx = self.context
+      cats = []
+
+      for cog, cmds in mapping.items():
+        if cog and await self.filter_commands(cmds, sort=True):
+          cats.append(cog)
+
+      embed = discord.Embed(color=discord.Color(COLOR))
+      for idx in cats:
+        embed.add_field(
+          inline=False,
+          name=idx.qualified_name.title(),
+          value=", ".join(map(lambda x: f"`{x}`", filter(lambda x: not x.hidden, idx.get_commands()))),
+            )
+
+        embed.set_footer(text="made with 💖 by Aendon")
+        await ctx.send(embed=embed)
+
+    async def send_command_help(self, command):
+      embed = self.common_command_formatting(command)
+      await self.context.send(embed=embed)
